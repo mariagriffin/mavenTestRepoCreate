a717d0c470299577cd338818859b760373dffe71
mgriffin <maria.griffin@atu.ie>
2025-10-16 14:58:17 +0100

    commit

---
+++ mvnApp/src/main/java/ie/atu/MvnApps/App.java
@@ -0,0 +1,13 @@
+package ie.atu.MvnApps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
