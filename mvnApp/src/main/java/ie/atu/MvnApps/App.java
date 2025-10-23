de3f75ae231f315ee484327a674238b9da15c8a6
mgriffin <maria.griffin@atu.ie>
2025-10-23 14:04:21 +0100

    code change

--- mvnApp/src/main/java/ie/atu/MvnApps/App.java
+++ mvnApp/src/main/java/ie/atu/MvnApps/App.java
@@ -1,4 +1,4 @@
-package ie.atu.MvnApps;
+|package ie.atu.MvnApps;
 
 /**
  * Hello world!
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "This is a code change" );
     }
 }
